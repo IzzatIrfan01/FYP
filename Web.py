fa1155a63329b0de0121e6efa11e9f7d26791291
IzzatIrfan01 <127728809+IzzatIrfan01@users.noreply.github.com>
2024-01-03 17:37:08 +0800

    Update Web.py

--- Web.py
+++ Web.py
@@ -5,8 +5,7 @@ import joblib
 import plotly.graph_objects as go
 from datetime import datetime, timedelta
 import logging
-from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF, Matern, ConstantKernel as C
+import sklearn
 
 # Create a logger
 logger = logging.getLogger(__name__)
